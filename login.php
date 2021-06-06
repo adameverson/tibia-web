ab55142872a28237de82bf106e1d9de8273edca5
Adam Everson Souza Araújo <adameversons@gmail.com>
2021-06-06 19:34:18 -0300

    Update version

--- login.php
+++ login.php
@@ -234,5 +234,5 @@
 </audio>
 
 <div style="text-align: center; font-family: 'Lucida Console', 'Courier New', monospace;">
-    ©2021 magiclevel.ml. All rights reserved. v1.2.20
+    ©2021 magiclevel.ml. All rights reserved. v1.2.23
 </div>
